aa7e2dd29873b34be7f356d957bf916a26a080a9
Evan Hearne <evanhearne02@gmail.com>
2023-10-24 13:12:15 +0100

    Added Upcoming movies page.

---
+++ movies/src/api/tmdb-api-upcoming.js
@@ -0,0 +1,45 @@
+export const getMovies = () => {
+    console.log(`https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`)
+    return fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+    )
+      .then(res => res.json())
+      .then(json => json.results);
+  };
+  
+  export const getMovie = id => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/upcoming/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    ).then(res => res.json());
+  };
+  
+  export const getGenres = () => {
+    return fetch(
+      "https://api.themoviedb.org/3/movie/upcoming/list?api_key=" +
+        process.env.REACT_APP_TMDB_KEY +
+        "&language=en-US"
+    )
+      .then(res => res.json())
+      .then(json => json.genres);
+  };
+  
+  export const getMovieImages = (id) => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/upcoming/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    )
+      .then((res) => res.json())
+      .then((json) => json.posters);
+  };
+
+  export const getMovieReviews = (id) => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/upcoming/${id}/reviews?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        // console.log(json.results);
+        return json.results;
+      });
+  };
+
+  
--- movies/src/index.js
+++ movies/src/index.js
@@ -5,6 +5,7 @@ import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
+import DiscoverPage from "./pages/discoverPage";
 import SiteHeader from './components/siteHeader'
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
         <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/movies/upcoming" element={<DiscoverPage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={ <Navigate to="/" /> } />
       </Routes>
