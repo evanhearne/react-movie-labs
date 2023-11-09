242e1b3d91725c12e14441d281dc1c45258bee3e
Evan Hearne <evanhearne02@gmail.com>
2023-11-09 10:55:26 +0000

    Change upcoming movies page to use caching.

--- movies/src/pages/discoverPage.js
+++ movies/src/pages/discoverPage.js
@@ -3,11 +3,11 @@ import { getMovies } from "../api/tmdb-api-upcoming";
 import PageTemplate from '../components/templateMovieListPage'
 import { useQuery } from 'react-query';
 import Spinner from '../components/spinner';
-import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
 import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
 
 const DiscoverPage = (props) => {
 
+  // caching was added previously - foresaw the future here!
   const {  data, error, isLoading, isError }  = useQuery('discover', getMovies)
 
   if (isLoading) {
